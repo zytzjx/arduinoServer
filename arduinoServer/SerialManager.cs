1c90eda14a5c55c8b54ba6bc1eb10506a1a31013
zytzjx <zytzjx@sina.com>
2020-09-22 21:32:09 -0700

    fixed index

--- arduinoServer/SerialManager.cs
+++ arduinoServer/SerialManager.cs
@@ -347,7 +347,7 @@ namespace arduinoServer
             bool bret = false;
             int label = Convert.ToInt32(inpt["label"]) -1;
 
-            int gg = (label - 1) / GroupCnt;
+            int gg = label / GroupCnt;
             int ggmod = label % GroupCnt;
 
             int[] ledindexs = ((Object[])config["stripindexs"]).Cast<int>().ToArray();
